93cfb6c4e5f57c5e07630e15e8bf9359e502c0d1
codenameraziel <71160295+codenameraziel@users.noreply.github.com>
2026-02-27 23:03:47 -0300

    up 2.0

--- meetecho/janus/html/demos/janus.js
+++ meetecho/janus/html/demos/janus.js
@@ -592,7 +592,7 @@ var Janus = (function (factory) {
 		let wsKeepaliveTimeoutId = null;
 		let servers = null;
 		let serversIndex = 0;
-		let server = [ "http://192.168.100.105:8088/janus", "ws://192.168.100.105:8188" ];
+		let server = [ "http://192.168.100.105:8088/janus", "ws://umbrel.tailc9398a.ts.net:8289" ];
 		if(Janus.isArray(server)) {
 			Janus.log("Multiple servers provided (" + server.length + "), will use the first that works");
 			servers = server;
